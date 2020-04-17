00db98441a88dd35c4772c992257f9e7376b71e8
Aziz Alarfaj <azs.alarfaj@gmail.com>
2020-04-17 17:18:59 +0300

    first changes

--- CPU.java
+++ CPU.java
@@ -1,69 +1,90 @@
 public class CPU extends Thread {
-private Process excutingProcess;
-private int upTime;
-private int downTime;
+	private Process excutingProcess;
+	private int busyTime;
+	private int idelTime;
 
-public CPU () {
-	excutingProcess = null;
-	upTime = downTime = 0;
-}
-public void run() {
-	while(true) {
-		this.excutingProcess = RAM.readyQ.serve();
-		if(excutingProcess !=null) {
-			excutingProcess.setState(STATE.running);
-		excute();
-		}else {
-			try {
-				sleep(5);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	public CPU() {
+		this.excutingProcess = null;
+		this.busyTime = 0;
+		this.idelTime = 0;
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			this.excutingProcess = RAM.readyQ.serve();
+			if (excutingProcess != null) {
+				excuteProcess();
+			} else {
+				try {
+					sleep(5);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				idelTime++;
+				System.out.println("down time " + idelTime);
+				Clock.incrementClock();
+				System.out.println("clock time " + Clock.currentTime);
 			}
-			downTime++;
-			System.out.println("down time "+downTime);
-			Clock.increment(1);
-			System.out.println("clock time "+Clock.currentTime);
 		}
 	}
-}
-public void excute() {
-	Brust currentBrust = excutingProcess.getCurrentB();
-	while(currentBrust.getRemainingTime()>0) {
-		currentBrust.decrementRemainingTime();
-		System.out.println("ecxuting prcess "+excutingProcess.getName()+"current burst remaining time "+ currentBrust.getRemainingTime());
-		excutingProcess.incrmentCPUtime();
-		upTime++;
-		System.out.println("up time "+upTime);
-		Clock.increment(1);
-		currentBrust = checkReadyQueue(currentBrust);
-	}
-	excutingProcess.nextBrust();
-	if(excutingProcess.getCurrentB() instanceof IOBrust) {
-		//add to IOwaiting
-	}else {
-		if(excutingProcess.getCurrentB().getRemainingTime()==-1) {
-			excutingProcess.terminateProcess();
-		}else {
-			RAM.addToReadyQueue(excutingProcess);
+
+	// handle the process in the CPU
+	public void excuteProcess() {
+		excutingProcess.setState(STATE.running); // set state to running
+
+		Burst currentBurst = excutingProcess.getCurrentBurst(); // get the first burst of the process
+		// increment cpu counter/uses??
+
+		while (currentBurst.getRemainingTime() > 0) {
+			Clock.incrementClock();
+			excutingProcess.incrmentCPUtime();
+			busyTime++;
+			currentBurst.decrementRemainingTime();
+
+			// test case ---------------------------------------
+			if (Test.TEST_MODE)
+				System.out.println("ecxuting prcess " + excutingProcess.getName() + "current burst remaining time "
+						+ currentBurst.getRemainingTime());
+			System.out.println("up time " + busyTime);
+			// -------------------------------------------------
+
+			currentBurst = checkReadyQueue(currentBurst);
+			// sleep??
+		}
+
+		excutingProcess.nextBurst();
+		if (excutingProcess.getCurrentBurst() instanceof IOBurst) {
+			// add to IOwaiting
+		} else {
+			if (excutingProcess.getCurrentBurst().getRemainingTime() == -1) {
+				excutingProcess.terminateProcess();
+			} else {
+				RAM.addToReadyQueue(excutingProcess);
+			}
 		}
 	}
-}
-public Brust checkReadyQueue(Brust current) {
-	if(RAM.readyQ.peek() != null &&RAM.readyQ.peek().data.getCurrentB().getRemainingTime()<current.getRemainingTime()) {
-		excutingProcess.incrementNumberOfPreemptions();
-		excutingProcess.setState(STATE.ready);
-		RAM.readyQ.enqueue(current.getRemainingTime(), excutingProcess);
-		excutingProcess = RAM.readyQ.serve();
-		excutingProcess.setState(STATE.running);
-		return excutingProcess.getCurrentB();
+
+	// close from the shortTimeScheduler idea
+	public Burst checkReadyQueue(Burst current) {
+		// check if there is a shorter burst
+		if (RAM.readyQ.peek() != null
+				&& RAM.readyQ.peek().data.getCurrentBurst().getRemainingTime() < current.getRemainingTime()) {
+			excutingProcess.incrementNumberOfPreemptions();
+			excutingProcess.setState(STATE.ready);
+			RAM.readyQ.enqueue(current.getRemainingTime(), excutingProcess); // return the currentBurst to the readyQ
+			excutingProcess = RAM.readyQ.serve(); // change to the shorter burst
+			excutingProcess.setState(STATE.running);
+			return excutingProcess.getCurrentBurst();
+		}
+		return current;
 	}
-	return current;
-}
-public int getUpTime() {
-	return upTime;
-}
-public int getDownTIme() {
-	return downTime;
-}
+
+	public int getbusyTime() {
+		return busyTime;
+	}
+
+	public int getidelTime() {
+		return idelTime;
+	}
 }
